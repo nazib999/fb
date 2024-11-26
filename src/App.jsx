b3345fa885187abe9cb41e1d31b03e2c9d94b14c
nazib ullah <najibullahnajib758@gmail.com>
2024-11-27 01:54:10 +0600

    initial-comt

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/fb/'>
       <Routes>
 
           <Route index element={<Home />} />
